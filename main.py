2b85a07500ce2638df03c0010ba2f7bd819cf3b4
Abdullah Khetran <khetranabdullah@gmail.com>
2025-09-20 04:13:58 +0500

    added cors for frontend

--- main.py
+++ main.py
@@ -1,11 +1,25 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from typing import Dict, Any
-
 from app.agent_logic import analyze_topic
 
 
 app = FastAPI()
+
+# CORS setup
+origins = [
+    "https://ioa-sa-polarization-mapper.streamlit.app",  # deployed frontend
+    "http://localhost:8501"  # local frontend testing
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 class TopicRequest(BaseModel):
